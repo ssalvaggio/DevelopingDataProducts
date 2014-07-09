47f61172cc22540d37c649ca50e729b27dad928a
ssalvaggio <salvino.salvaggio@gmail.com>
2014-07-09 18:26:22 +0300

    Shiny files

---
+++ server.R
@@ -0,0 +1,50 @@
+library(shiny)
+data <- read.csv('fitbitData.csv', header=TRUE, na.strings = 'NA')
+fit <- lm(Cal.Burned ~ V.Active.Min, data=data)
+    
+shinyServer(function(input, output) {
+    
+    # Declare 'variables', 'Plot', 'predictor', 'df' and 'predicted' as reactive 
+    # expressions, sot they will be called only when/if the input they depend on changes
+    variables <- reactive({ as.numeric(input$variables) })
+    Plot <- reactive({ as.numeric(input$plot) })
+    predictor <- reactive({ as.numeric(input$predictor) })
+    df <- reactive({ data.frame(V.Active.Min=predictor()) })
+    predicted <- reactive({ round(as.numeric(predict(fit, newdata=df())),0) })
+    
+    # Generate head(n) of the dataset
+    output$head <- renderTable({ 
+        n_rows = as.numeric(input$n_rows)
+       head(data, n_rows)
+    })
+    
+    # Generate a summary of the requested variables
+    output$summary <- renderPrint({
+         # summary(data[, variables()])
+        if(length(variables() > 0)) {summary(data[, variables()])}
+           else {
+         cat('At least one variable must be checked to see a summary')}
+    })
+    
+    output$boxPlot <- renderPlot({
+        boxplot(data[Plot()], main="Data distribution", xlab=names(data[Plot()]))
+    })
+    
+    output$predictedValue <- renderUI({
+        HTML(paste('If, on a given day, the number of active minutes is <font color=\"blue\"><b>', 
+                       predictor(), '</b></font> then, the predicted number of 
+                    calories burned that day by that person is  <font color=\"blue\"><b>', 
+                    predicted(), '</b></font>.' ))
+    })
+
+    output$scatterPlot <- renderPlot({
+        with(data, plot(V.Active.Min, Cal.Burned, xlim=c(0,205), ylim=c(1750, 3500),
+                          main='Calories burned vs. very active Minutes',
+                          ylab='Calories burned', xlab='Very active minutes (daily)'))
+        abline(fit, col='grey55')
+        points(predictor(), predicted(), pch=19, col='blue', cex=1.7)
+        legend('bottomright', 'predicted point', col='blue', pch=19)
+    
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,91 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel('Monitoring of Daily Physical Activity and Calories Burned'),
+    p('Salvino A. Salvaggio, PhD - July 2014'),br(),
+    # Info
+    h4('Supporting documentation'),
+        p('The data analyzed come from a Fitbit activity monitor and Fitbit 
+            app installed on a smartphone. Each row provides the daily figures for several variables 
+            collected on one single individual over the period 24 Nov 2013 - 10 May 2014. 
+            The dataset used here is a subset of a much larger dataset (many more variables).
+            Complete dataset and info available ', 
+            a(href='https://dl.dropboxusercontent.com/u/278538/Fitbit_data_20140621.zip', 'here'),'.'),
+        p('To interact with this page, you can use the widgets in the lefthand column: 
+             \'Overview\' allows to select how many rows of the dataset you want to preview;
+            in \'Summary\' you can choose the variables to summarize and display; \'Boxplot\' 
+            and the related Boxplot tab let you choose which variable to see as a boxplot; and \'Prediction\'
+            requires a number of minutes as a predictor and predicts the expected daily calories
+            burned accordingly and plot the outcome.'),
+    br(),
+    
+    sidebarLayout(
+        sidebarPanel(
+            # Input number of rows to visualize        
+                    h4('Overview'),
+                    selectInput(inputId = 'n_rows', h5('Number of observations to view:'), 
+                                c('5 rows' = 5, '10 rows' = 10, '20 rows' = 20), 
+                                selected=5),
+                    HTML('<hr style="border-color: lightgrey;">'),
+            
+            # Select variables to appear in summary
+                    h4('Summary'),
+                        checkboxGroupInput('variables', 
+                                        label = h5('Choose variable(s) to be summarized:'), 
+                                        c('Calories In'= 2, 'Calories Burned' = 3, 'Daily Steps' = 4, 
+                                          'Walked Distance (km)' = 5,
+                                          'Very Active Minutes' = 6),
+                                        selected = 2:4),
+                    HTML('<hr style="border-color: lightgrey;">'),
+
+            # Select variables to be plot
+            h4('Boxplot'),
+            radioButtons('plot', 
+                               label = h5('Choose variable to be plotted:'), 
+                               c('Calories In'= 2, 'Calories Burned' = 3, 'Daily Steps' = 4, 
+                                 'Walked Distance (km)' = 5,
+                                 'Very Active Minutes' = 6),
+                               selected = 2),
+            HTML('<hr style="border-color: lightgrey;">'),
+            
+            # Choose number of mins to feed prediction model (lm)
+                    h4('Prediction'),
+                    sliderInput('predictor', label = h5('Please, provide the number of ', em('very 
+                                                      active minutes,'), 'then the model 
+                                                      predicts the estimated
+                                                      calories burned that day:'),
+                                min = 0, max = 200, value = 60)
+                      ),
+        
+        mainPanel(
+            # Display output: dataset
+                h4('Overview'),
+                tableOutput('head'),
+
+            HTML('<div style=\'background-color:#FAFAFA; padding:15px;\'>'),
+
+            # Display output: summary stats
+
+                tabsetPanel(type = 'tabs', selected = 'Summary',
+                            tabPanel('Summary',
+                                     h4('Summary'),
+                                     br(),
+                                     verbatimTextOutput('summary')
+                                     ),
+                            tabPanel('Boxplot', 
+                                     h4('Boxplot'),
+    
+                                     plotOutput('boxPlot'))
+                            ),
+            
+            HTML('</div>'),
+            br(),
+            
+            # Display output: prediction based on lm
+                h4('Prediction'),
+                uiOutput('predictedValue'),
+                plotOutput('scatterPlot')   
+        )
+                    
+    )
+))
